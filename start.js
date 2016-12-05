10841d4c8a6eaab6856d405cb3420a2f14627661
Meegul304 <micha3lbilstein@gmail.com>
2016-12-05 15:37:53 -0500

    Remove unnecessary features

--- start.js
+++ start.js
@@ -2,17 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
-require("dotenv").config();
 
 const port = process.env.PORT || 3000;
-
-
-const token = process.env.TOKEN;
 const botID = process.env.BOT_ID;
 const url = "https://api.groupme.com/v3/bots/";
 const groupID = process.env.GROUP_ID;
-let admin = process.env.ADMIN_ID;
-let richard = process.env.RICHARD;
 
 
 app.use(bodyParser.json());
@@ -25,8 +19,8 @@ app.post("/", (req, res) => {
     const toSearchFor = "@giphy ";
     const index = req.body.text.toLowerCase().indexOf(toSearchFor);
     
-    //If we found the string we're looknig for, and they're not richard, get results and send them
-    if (index != -1 && req.body.user_id !== richard) {
+    //If we found the string we're looking for get results and send them
+    if (index != -1) {
         //Get the string we are searching for.
         const toSearch = req.body.text.substring(index + toSearchFor.length);
         
@@ -34,6 +28,7 @@ app.post("/", (req, res) => {
         const giphyurl = `http://api.giphy.com/v1/gifs/search?limit=1&q=${encodeURIComponent(toSearch)}&api_key=dc6zaTOxFJmzC`;
         
         request.get(giphyurl, (error, response, body) => {
+            //If there was an error, or no gifs were found
             if (error || !JSON.parse(body)["data"][0]) {
                 sendMessage(botID, "Nothing found :(");
             } else if (body) {
@@ -43,27 +38,11 @@ app.post("/", (req, res) => {
             }
         });
     }
-    //If the user is the admin
-    if (req.body.user_id === `${admin}`) {
-        const commandText = req.body.text.split("=");
-        //If they had an '=' in their message, it must be a command
-        if (commandText.length > 1) {
-            //If the text before = is 'richard', reassign richard to what comes after the '='
-            if (commandText[0] === "richard")
-                richard = commandText[1];
-        }
-    }
-    //If the user is richard, just make the text "No, Richard."
-    if (req.body.user_id === richard)
-        postText = "No, Richard.";
-    //If we have any text to send, send it.
-    if (postText)
-        sendMessage(botID, postText);
     res.end();
 });
 
 function sendMessage(botID, text) {
-    const toSend = `${url}post?bot_id=${botID}&text=${encodeURIComponenent(text)}`;
+    const toSend = `${url}post?bot_id=${botID}&text=${encodeURIComponent(text)}`;
     request.post(toSend, (error, response, body) => {
         if (error) {
             console.log(error);
